828d36c8a70759be0aeef8cb4eded1993d7c21c1
unknown <hitprofit70@gmail.com>
2023-03-08 09:13:42 +0200

    cartbag fixed

--- src/pages/CartBag.jsx
+++ src/pages/CartBag.jsx
@@ -9,21 +9,22 @@ const CartBag = () => {
       <Navbar />
       <div className="container mt-5">
         <h4>Shopping Cart</h4>
-        <div className="shopping-card mt-4">
-          <div className="cart-logo">
-            <img
-              src="https://th.bing.com/th/id/OIP.Axz-vFaD-7hHlbO7r6ZGkQHaIu?pid=ImgDet&rs=1"
-              width="130"
-              height="130"
-              alt=""
-            />
+        <div className="shopping-card">
+          <div className="shopping-cart">
+            <div className="shopping-img">
+              <img
+                src="https://th.bing.com/th/id/OIP.Axz-vFaD-7hHlbO7r6ZGkQHaIu?pid=ImgDet&rs=1"
+                width="130"
+                height="130"
+                alt=""
+              />
+            </div>
+            <span className="shopping-text">Your shopping cart is empty</span>
+            <br></br>
+            <Link to="/">
+              <button className="shopping-btn">Continue Shopping</button>
+            </Link>
           </div>
-          <span className="text">
-            Your shopping cart is empty
-          </span><br></br>
-          <Link to="/"> 
-          <button className="shopping-btn">Continue Shopping</button>
-          </Link>
         </div>
       </div>
       <Footer />
