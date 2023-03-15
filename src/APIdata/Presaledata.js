704c4b04282de08803456e07d1bef77355208504
unknown <hitprofit70@gmail.com>
2023-03-15 09:06:36 +0200

    data file added

--- src/APIdata/Presaledata.js
+++ src/APIdata/Presaledata.js
@@ -2,58 +2,66 @@ const saledata = [
     {
       id: 9,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2022/06/13/165511883296f804af8aacc5a76253e17a9672a9bd_thumbnail_900x.webp",
+      title: "Lantern Sleeve Drawstring",
+      discountprice: 199.99,
+      amount: 266.99,
     },
     {
       id: 10,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2021/08/05/162813408375956981a4c459bedfc41014840bf387_thumbnail_900x.webp",
+      title: " Rib-knit Solid Top",
+      discountprice: 149.99,
+      amount: 189.99,
     },
     {
       id: 11,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2021/09/07/163101145243252452727da495774e1e5a208b09eb_thumbnail_810x.webp",
+      title: "Drop Shoulder Twist Tee",
+      discountprice: 289.99,
+      amount: 325.99,
     },
     {
       id: 12,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2021/12/20/1639967440bb37ddd83551bf163ed1172412c4dbbd_thumbnail_900x.webp",
+      title: "Striped Drop Shoulder",
+      discountprice: 159.99,
+      amount: 199.99,
     },
     {
       id: 13,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2022/05/13/16524244914112e4a06dcfedc5f2ca1220d6cfd77b_thumbnail_900x.webp",
+      title: "Contrast Raglan Sleeve ",
+      discountprice: 145.99,
+      amount: 189.99,
     },
     {
       id: 14,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2022/08/05/16596888129cef1d582897f46779f018ad247cc975_thumbnail_900x.webp",
+      title: "Japanese Letter Graphic Tee",
+      discountprice: 257.99,
+      amount: 299.99,
     },
     {
       id: 15,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2022/09/01/1662010090de9b0121c470c0eacf90aedff72fcfce_thumbnail_900x.webp",
+      title: "Print Skinny Pants",
+      discountprice: 234.99,
+      amount: 295.99,
     },
     {
       id: 16,
       Img:
-        "https://img.ltwebstatic.com/images3_pi/2022/11/15/166848256291b21fc7b6add14d531765fbb95dbfd0_thumbnail_900x.webp",
-      title: "LB Denim Skirt",
-      amount: 454.99,
+        "https://img.ltwebstatic.com/images3_pi/2022/10/09/16652841563c0b70a39553bedbed91b101886c1166_thumbnail_900x.webp",
+      title: "Slant Pocket Skinny Pants",
+      discountprice: 214.99,
+      amount: 289.99,
     },
   ];
   
--- src/APIdata/Womendata.js
+++ src/APIdata/Womendata.js
@@ -2,58 +2,86 @@ const productwomen = [
     {
         id: 29,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2023/01/15/16737683830e2ba902d51f2852cb6f0aafb288d81c_thumbnail_900x.webp",
+        title: "Allover Print Button Up",
+        amount: 212.99,
       },
       {
         id: 30,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2023/03/13/1678675209d7befc4a763e8e61b76fa8559cf8b687_thumbnail_900x.webp",
+        title: "Floral Print Bodycon",
+        amount: 139.99,
       },
       {
         id: 31,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2023/02/20/1676858825d3e444cbe246e9d21aeae709879b5789_thumbnail_900x.webp",
+        title: "Belted Halter",
+        amount: 224.99,
       },
       {
         id: 32,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2022/11/05/1667634352175ec46abe56a5aa4a34acf3f3bfdcfb_thumbnail_900x.webp",
+        title: "Backless Ruched Mesh",
+        amount: 149.99,
       },
       {
         id: 33,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2021/10/05/1633423947fa6cc9dd079c3a6f2b812df8dd199956_thumbnail_900x.webp",
+        title: "Mock Neck Thermal Lined",
+        amount: 129.99,
       },
       {
         id: 34,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2022/06/29/165649255766d6cff725bdfb5ccdf117fc6d31fb34_thumbnail_900x.webp",
+        title: "Thermal Lined Sweatshirt",
+        amount: 169.99,
       },
       {
         id: 35,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2022/09/09/16627059088aa6954883330054a59b61f086ce108b_thumbnail_900x.webp",
+        title: "Thermal Lined Sweatshirt",
+        amount: 173.99,
       },
       {
         id: 36,
         Img:
-          "https://img.ltwebstatic.com/images3_pi/2021/11/03/1635910345693d07afb3a754b2b28b34ff17fc7f6e_thumbnail_900x.webp",
-        title: "Yellow Denim",
-        amount: 354.99,
+          "https://img.ltwebstatic.com/images3_pi/2022/10/28/16669433953afaab1df9dfcfb1c9dd9308fe81823d_thumbnail_900x.webp",
+        title: "Coffee And Slogan Graphic",
+        amount: 157.99,
+      },
+      {
+        id: 37,
+        Img:
+          "https://img.ltwebstatic.com/images3_pi/2023/02/21/16769428448b842271c87d16dba4510970db2930e3_thumbnail_900x.webp",
+        title: "High Waist Carrot Jeans",
+        amount: 246.99,
+      },
+      {
+        id: 38,
+        Img:
+          "https://img.ltwebstatic.com/images3_pi/2023/02/21/16769428554c95f2f311847659986d2b861a2ef707_thumbnail_405x552.webp",
+        title: "Zip Fly Skinny Jeans",
+        amount: 356.99,
+      },
+      {
+        id: 39,
+        Img:
+          "https://img.ltwebstatic.com/images3_pi/2022/05/30/1653887930c7a9d1271ebe3f35ab8742f605c56698_thumbnail_900x.webp",
+        title: "Dark Wash Skinny Jeans",
+        amount: 291.99,
+      },
+      {
+        id: 40,
+        Img:
+          "https://img.ltwebstatic.com/images3_pi/2023/02/13/1676250807cf65090b0633d363b4dd946c28bbf01d_thumbnail_900x.webp",
+        title: "Paperbag Waist Mom Fit Jeans",
+        amount: 335.99,
       },
 ]
 
